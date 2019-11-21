9d87758728fa3ba2b7b967b3a0c59db1314ea082
Reyna Neet <reyna.neet@gmail.com>
2019-11-21 16:50:34 -0600

    Moved buttons into their own component

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,24 @@
 <template>
   <div class="container">
     <div>
-      <!-- <logo /> -->
       <h1 class="title">
         Reyna Neet
       </h1>
-      <!-- <h2 class="subtitle">
-        I haven't decided what to do with this yet!
-      </h2> -->
       <div class="links">
-        <a href="https://linkedin.com/in/ryan-neet/" target="_blank" class="button">
-          LinkedIn
-        </a>
-        <a
-          href="https://github.com/reyna-neet/"
-          target="_blank"
-          class="button"
-        >
-          Github
-        </a>
-        <a href="https://twitter.com/reynaneet" target="_blank" class="button">
-          Twitter
-        </a>
+        <Button url="https://linkedin.com/in/ryan-neet/" name="LinkedIn" />
+        <Button url="https://github.com/reyna-neet/" name="GitHub" />
+        <Button url="https://twitter.com/reynaneet" name="Twitter" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import Logo from '~/components/Logo.vue'
+import Button from '~/components/Button.vue'
 
 export default {
   components: {
-    // Logo
+    Button 
   }
 }
 </script>
