1c1c3fa9f460c0c69a855e9e873eaab0ba977b1f
Reyna Neet <reyna.neet@gmail.com>
2019-11-21 21:06:06 -0600

    Added blog page and basic list function

---
+++ pages/blog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Blog 
+      </h1>
+      <ul id="index">
+        <li v-for="post in posts">
+          <Button :url="post.pathLong" :name="post.title" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from '~/components/Button.vue'
+
+export default {
+  data: function() {
+    return {
+      posts: [
+        { pathLong: 'https://linkedin.com/in/ryan-neet/',  title: 'LinkedIn' },
+        { pathLong: 'https://twitter.com/reynaneet/', title: 'Twitter' }
+      ]
+    };
+  },
+  components: {
+    Button
+  }
+}
+</script>
