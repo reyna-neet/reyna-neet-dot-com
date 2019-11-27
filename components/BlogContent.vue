16b0baf2e1bd0510679decfccf51cf646bae71e0
Reyna Neet <reyna.neet@gmail.com>
2019-11-26 20:58:09 -0600

    Blog post pages now display unstyled ipsum text

---
+++ components/BlogContent.vue
@@ -0,0 +1,8 @@
+<script>
+import Content from '../assets/posts/ipsum.md'
+
+export default {
+  extends: Content.vue.component
+}
+
+</script>
--- pages/blog/_post.vue
+++ pages/blog/_post.vue
@@ -2,15 +2,19 @@
   <div class="container">
     <h1 class="title"> {{ $route.params.post }} </h1>
     <client-only placeholder="Loading...">
-      <Content.vue.component />
+      <BlogContent />
     </client-only>
   </div>
 </template>
 
 <script>
-import Content from '../../assets/posts/ipsum.md'
+import BlogContent from '../../components/BlogContent.vue'
 
 export default {
+  name: 'post',
+  components: {
+    BlogContent
   }
+}
 
 </script>
