62f606b25c44680e0285dccbb23521ff3e8522af
Reyna Neet <reyna.neet@gmail.com>
2019-12-05 12:47:31 -0600

    Button component can now be toggled to internal or external via prop

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,9 @@
         Reyna Neet
       </h1>
       <div class="links">
-        <Button url="https://linkedin.com/in/ryan-neet/" name="LinkedIn" />
-        <Button url="https://github.com/reyna-neet/" name="GitHub" />
-        <Button url="https://twitter.com/reynaneet" name="Twitter" />
+        <Button url="https://linkedin.com/in/ryan-neet/" name="LinkedIn" external="true" />
+        <Button url="https://github.com/reyna-neet/" name="GitHub"  external="true"/>
+        <Button url="https://twitter.com/reynaneet" name="Twitter"  external="true"/>
       </div>
     </div>
   </div>
