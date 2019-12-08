89c722568a43451aafb7f7dd7c916064c8c44de6
Reyna Neet <reyna.neet@gmail.com>
2019-12-08 15:28:38 -0600

    Added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,17 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
-      }
+      },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffdd55' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { 
+      { rel: 'apple-touch-icon', sizes:'180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ffdd55' },
+      {
         rel: 'stylesheet', media: 'screen',
         href: 'https://fontlibrary.org/face/at-night',
         type: 'text/css'
